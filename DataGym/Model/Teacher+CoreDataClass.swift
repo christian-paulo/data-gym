83767fbb48270dba20f3a3ad4fe9fb0a56040208
NarelyLima <narely148@gmail.com>
2022-06-07 11:59:19 -0300

    Create the CRUD

---
+++ DataGym/Model/Teacher+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Teacher+CoreDataClass.swift
+//  DataGym
+//
+//  Created by Narely Lima on 07/06/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Teacher)
+public class Teacher: NSManagedObject {
+
+}
--- DataGym/ViewController.swift
+++ DataGym/ViewController.swift
@@ -6,41 +6,161 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     
     @IBOutlet var tableView: UITableView!
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var items: [Class]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ClassCell")
+
+        
+        fetchClass()
     }
-
-
     
-}
+    func fetchClass() {
+        do {
+            self.items = try context.fetch(Class.fetchRequest())
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        catch {
+            
+        }
+        
+    }
+    
+    @IBAction func addTapped(_sender: Any){
 
-extension ViewController: UITableViewDelegate{
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        print("you tapped me!")
+        
+        let alert = UIAlertController(title: "Add Class", message: "Whats ir their name?", preferredStyle: .alert)
+        alert.addTextField()
+        
+        let submitButton = UIAlertAction(title: "Add", style: .default) {
+            (action) in
+            
+            let textfield = alert.textFields![0]
+            
+            // TODO: Create a class object
+            
+            let newClass = Class(context: self.context)
+            newClass.name = textfield.text
+            newClass.day_week = "Segunda-feira"
+            newClass.hour = "8 as 10"
+            newClass.semester_school = "2020.1"
+            
+            
+            //TODO: save to data
+            
+            do{
+                try self.context.save()
+            }
+            catch{
+                
+            }
+            //re-fecth the data
+            
+            self.fetchClass()
+
+        }
+        alert.addAction((submitButton))
+                
+        self.present(alert, animated: true, completion: nil)
+        
     }
 }
 
-extension ViewController: UITableViewDataSource {
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let action = UIContextualAction(style: .destructive, title: "Delete") { (action,
+                                                                                 view, completionHandler) in
+            
+            //TODO: WHICH CLASS TO REMOVE
+            let classToRemove =  self.items![indexPath.row]
+            
+            //TODO: REMOVE THE CLASS
+            self.context.delete(classToRemove)
+            
+            //TODO: SAVE THE DATA
+            do {
+                try self.context.save()
+            }
+            catch {
+                
+            }
+            
+            //TODO: RE-FETCH THE DATA
+            self.fetchClass()
+        }
+        
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return self.items?.count ?? 0
     }
-    func tableView(_ tableview: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-    let cell = tableView.dequeueReusableCell(withIdentifier:"cell",for:indexPath)
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         
-        cell.textLabel?.text = "HelloWorld"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ClassCell", for:indexPath)
         
+        let classe = self.items![indexPath.row]
+        
+        
+        cell.textLabel?.text = classe.name
         return cell
-    
     }
     
+    func tableView (_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let classe = self.items![indexPath.row]
+        
+        let alert = UIAlertController(title: "Edit Person", message: "Edit name: ", preferredStyle: .alert)
+        alert.addTextField()
+        
+        let textfield = alert.textFields![0]
+        textfield.text = classe.name
+        
+        let saveButton = UIAlertAction(title: "Save", style: .default) { (action) in
+            
+            //GET THE TEXTFIELD FOR THE ALERT
+            let textfield = alert.textFields![0]
+            
+            //EDIT NAME PROPERTY OF CLASS OBJECT
+            classe.name = textfield.text
+
+            //SAVE THE DATA
+            
+            do {
+                try self.context.save()
+            }
+            catch {
+                
+            }
+            self.fetchClass()
+        }
+        alert.addAction(saveButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
+
+
+
+
 
