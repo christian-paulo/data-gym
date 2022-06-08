4f87d23cf6be91931d901bb5770a3f8a07f7c8cd
Ieda Xavier <92699834+Sunlower@users.noreply.github.com>
2022-06-08 09:38:00 -0300

    Turmas/Treinos

--- DataGym/ViewController.swift
+++ DataGym/ViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func mudardetela(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
